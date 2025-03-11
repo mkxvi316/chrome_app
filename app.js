2e95de60fb9c852fb98d9a444d5991bd83478af5
mkxvi16 <jihyeok316@naver.com>
2025-03-11 22:27:20 +0900

    3/11(화) 바닐라js #2.1 ~ #2.6 완료

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-alert("hi");
+// #2.3 const and let
+
+// const a = 5;
+// const b = 2;
+// let myName = "Michael"
+
+// console.log(a + b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log("hello " + myName);
+
+// myName = "mkxvi"
+
+// console.log("your new name is " + myName);
+
+//=================================================
+// #2.4 Booleans
+
+// const amIFat = null;
+// let something;
+// console.log(something, amIFat);
+
+//=================================================
+// #2.5 Arrays
+
+// const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+// // Get Item from Array
+// console.log(daysOfWeek);
+
+// // Add one more day to the array
+// daysOfWeek.push("sun");
+
+// console.log(daysOfWeek);
+
+//=================================================
+// #2.6 Objects
+
+const player = {
+    name : "mkxvi",
+    points : 10,
+    fat : true,
+};
+console.log(player);
+player.lastname = "potato";
+player.points += 15;
+console.log(player.points);
